f0fa847ee9015d8f3f59a88d7a8818a467f49c62
Van Quang <quanga4lh@gmail.com>
2023-09-11 23:25:54 +0700

    update remove_img: clean code, auto create folder save, add mode argument

--- Modules/remove_img.py
+++ Modules/remove_img.py
@@ -13,13 +13,14 @@ def create_folder_save(path_folder_save=r'Results_remove_imgs'):
         os.mkdir(os.path.join(path_folder_save, 'run0'))
         path_folder_save=os.path.join(path_folder_save, 'run0')
     except:
-        path_folder_save=os.path.join(path_folder_save, 'run' + str(int(os.listdir(path_folder_save)[-1].split('run')[-1])+1))
+        id=max(int(i.split('run')[-1]) for i in os.listdir(path_folder_save) if 'run' in i)+1
+        path_folder_save=os.path.join(path_folder_save, 'run' + str(id))
         os.mkdir(path_folder_save)
     path_images_save = os.path.join(path_folder_save, 'images')
     path_labels_save = os.path.join(path_folder_save, 'labels')
     create_folder(path_images_save, path_labels_save)
     return (path_images_save, path_labels_save)
-def remove_img(path_folder_data,path_folder_save=r'Results_remove_imgs', extension_img='.jpg', label_id=1):
+def remove_img(path_folder_data,path_folder_save=r'Results_remove_imgs', extension_img='.jpg', label_id=1, mode='copy'):
     path_imgs = os.path.join(path_folder_data, 'images')
     path_labels = os.path.join(path_folder_data,'labels')
     #get_file
@@ -37,21 +38,23 @@ def remove_img(path_folder_data,path_folder_save=r'Results_remove_imgs', extensi
                 if text.split(' ', 1)[0]==str(label_id):
                     label_success.append(label)
                     break
-    print(label_success)
     #create folder save
     path_images_save, path_labels_save = create_folder_save(path_folder_save)
     #move file
     success=0
     for label_file in label_success:
-        name_img = label_file.split('.')[0]+extension_img
-        print(name_img)
+        name_img = '.'.join(label_file.split('.')[:-1])+extension_img
         source_img_path = os.path.join(path_imgs, name_img)
         source_label_path = os.path.join(path_labels, label_file)
         if os.path.exists(source_img_path):
-            shutil.copy(source_img_path, path_images_save)
-            shutil.copy(source_label_path, path_labels_save)
+            if mode=='copy':
+                shutil.copy(source_img_path, path_images_save)
+                shutil.copy(source_label_path, path_labels_save)
+            elif mode=='move':
+                shutil.move(source_img_path, path_images_save)
+                shutil.move(source_label_path, path_labels_save)
+            else:
+                print('Error arguments: mode')
+                break
             success+=1
     print("Success:",success)
-def main():
-    remove_img(path_folder_data=r"C:/Users/PC/OneDrive - Hanoi University of Science and Technology/Desktop/New folder (2)")
-main()
